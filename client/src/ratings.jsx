a6ea37c013dcf93f9089bd0e5fcf9ff52eee4983
Windsvept <junsubjjang@gmail.com>
2018-09-14 18:14:52 -0700

    temp

--- client/src/ratings.jsx
+++ client/src/ratings.jsx
@@ -9,22 +9,27 @@ export default class Ratings extends React.Component {
   render() {
     return (
       <div id="modal" className={styles.modal} >
-        <div className={styles.ratings}>
-          <div className={styles.btnContainer}>
-            <button onClick={this.props.close} className={styles.close}>&times;</button>
+        <div className={styles.ratingsCont} >
+          <div onClick={this.props.close} className={styles.close}>&times;</div>
+          <div className={styles.ratings}>
+            <div className={styles.ratingsHeader}><h2>Rating Details</h2></div>
+            <div className={styles.ratingsBody} >
+              <div className={styles.monthlyTrend}>
+                <div className={styles.rateMidHeader}>
+                  <p>Monthly Trend</p>
+                  <div className={styles.rateMidYears}>
+                    <button>2018</button>
+                    <button>2017</button>
+                    <button>2016</button>
+                  </div>
+                </div>
+              </div>
+              <h3>Overall Rating</h3>
+              <div>Yelping Since {this.props.yelpingSince} with {this.props.ratings.amount} reviews</div>
+              <p>We calculate the overall star rating with randomly stored data!</p>
+              <p>Learn More.</p>
+            </div>
           </div>
-          <h1>Rating Details</h1>
-          <div>Monthly Trend
-            <button>2018</button>
-            <button>2017</button>
-            <button>2016</button>
-          </div>
-          <img src="https://cdn1.iconfinder.com/data/icons/line-essentials-88/20/4327-512.png" alt="Graph here for monthly trends..." min-height="300px" width="300px" />
-          <h3>Overall Rating</h3>
-          <div>Yelping Since {this.props.yelpingSince} with {this.props.ratings.amount} reviews</div>
-          <img src="https://cdn1.iconfinder.com/data/icons/line-essentials-88/20/4327-512.png" alt="Graph here for individual stars..." min-height="300px" width="300px" />
-          <p>We calculate the overall star rating with randomly stored data!</p>
-          <p>Learn More.</p>
         </div>
       </div>
     );

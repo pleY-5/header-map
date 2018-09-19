e887df26be77c7963ffd8b6215bb5227aa8ad8c0
Windsvept <junsubjjang@gmail.com>
2018-09-19 15:09:53 -0700

    from proxy

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -67,8 +67,8 @@ export default class Header extends React.Component {
   getData() {
     var id = window.location.pathname;
     id = id.substring(1, id.length - 1).toLowerCase();
-    $.get(`/${id}/res`, id, (response)=> {
-      console.log(`get request success to "/res" with restaurant name: ${id} data: `, response);
+    $.get(`http://localhost:7763/${id}/res`, id, (response)=> {
+      console.log(`get request success to "/res" with restaurant name or name: ${id} data: `, response);
       this.setState({
         name: response.name,
         restaurant: response,
@@ -141,9 +141,7 @@ export default class Header extends React.Component {
         </div>
         <Buttons />
       </div>
-      <div className={styles.subHeader}>
-        <Map address={this.state.address} city={this.state.city} state={this.state.state} postalCode={this.state.postalCode} latitude={this.state.latitude} longitude={this.state.longitude} tel={this.state.tel} url={this.state.url} />
-      </div>
+      <Map address={this.state.address} city={this.state.city} state={this.state.state} postalCode={this.state.postalCode} latitude={this.state.latitude} longitude={this.state.longitude} tel={this.state.tel} url={this.state.url} />
     </div>);
   }
 }
